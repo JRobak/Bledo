e40775ae76b90286d39c68e1b7c7c5b4b6ddc2c2
JRobak <oliwiawaszczuk12321@gmail.com>
2024-03-10 23:30:02 +0100

    View projects

--- routes/projects.py
+++ routes/projects.py
@@ -1,8 +1,39 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, render_template, redirect, url_for
+from databases.database_project import return_user_tables, check_exists, add_new_project as d
 
 projects_ = Blueprint('projects', __name__)
+
+
+@projects_.route('/projects/')
+def view_projects():
+    title = "Projekty"
+
+    user_id = request.cookies.get('user_id')
+    if user_id:
+        projects_list = return_user_tables(user_id)
+        return render_template('view_projects.html', title=title, projects_list=projects_list)
+
+    return redirect(url_for('login.login'))
+
+
+@projects_.route('/add_new_project/', methods=["POST"])
+def add_new_project():
+    if request.method == "POST":
+        user_id = request.cookies.get('user_id')
+        new_project_name = request.form.get('new_project_name')
+        if check_exists(new_project_name, user_id):
+            return view_project(new_project_name)
+        else:
+            d(new_project_name, user_id)
+            return view_project(new_project_name)
+
+    return redirect(url_for('account.account'))
 
 
 @projects_.route('/project/<project_name>/')
 def view_project(project_name):
     user_id = request.cookies.get('user_id')
+    if check_exists(project_name, user_id):
+        return render_template('view_project.html', project_name=project_name)
+
+    return redirect(url_for('account.account'))
