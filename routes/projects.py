97981fffb9ad7470cd329eeb00535704324baadb
JRobak <oliwiawaszczuk12321@gmail.com>
2024-03-11 14:57:58 +0100

    Update view projects

--- routes/projects.py
+++ routes/projects.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, request, render_template, redirect, url_for
-from databases.database_project import return_user_tables, check_exists, add_new_project as d
+from databases.database_project import return_user_tables, check_exists, add_new_project as aaad_new_project
 
 projects_ = Blueprint('projects', __name__)
 
 
+# WYSWIETLA WSZYSTKIE PROJEKTY
 @projects_.route('/projects/')
 def view_projects():
     title = "Projekty"
@@ -16,6 +17,7 @@ def view_projects():
     return redirect(url_for('login.login'))
 
 
+# POST - DODAJE NOWY PROJEKT
 @projects_.route('/add_new_project/', methods=["POST"])
 def add_new_project():
     if request.method == "POST":
@@ -24,12 +26,13 @@ def add_new_project():
         if check_exists(new_project_name, user_id):
             return view_project(new_project_name)
         else:
-            d(new_project_name, user_id)
+            aaad_new_project(new_project_name, user_id)
             return view_project(new_project_name)
 
     return redirect(url_for('account.account'))
 
 
+# WYSWIETLA PROJEKT O DANEJ NAZWIE
 @projects_.route('/project/<project_name>/')
 def view_project(project_name):
     user_id = request.cookies.get('user_id')
